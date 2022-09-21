544f96c6650d14e2de7812b33c0fb9634eaee652
Pawel <jawelg3@gmail.com>
2022-09-21 23:16:22 +0200

    Added scrollView

--- app/src/main/java/com/example/capitals/StartGame.java
+++ app/src/main/java/com/example/capitals/StartGame.java
@@ -17,12 +17,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
 import java.util.Objects;
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
 import me.grantland.widget.AutofitHelper;
 
@@ -33,9 +37,9 @@ public class StartGame extends AppCompatActivity {
 	boolean[] chosenCategories;
 	char chosenLetter;
 	TextView timeLeft;
-	ConstraintLayout conLay;
+	ScrollView gameView;
 	TableLayout tabLay;
-	static final int TIME_FOR_ANSWER = 15;
+	static final int TIME_FOR_ANSWER = 3;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -82,25 +86,37 @@ public class StartGame extends AppCompatActivity {
 
 	private void theGame() {
 
-		if(conLay == null) {
+		if(gameView == null) {
 			setContentView(R.layout.category_1);
-			TextView categoryTitle = findViewById(R.id.categoryPattern);
+
+			LinearLayout categoryTitleLayout = findViewById(R.id.category);
+
+			TextView categoryTitle = new TextView(this);
 			categoryTitle.setText("Countries");
-			conLay = findViewById(R.id.conLay);
+			categoryTitle.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1f));
+			categoryTitle.setTextSize(30);
+			categoryTitle.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+			categoryTitle.setTextColor(Color.BLACK);
+			categoryTitleLayout.addView(categoryTitle);
+
+			gameView = findViewById(R.id.gameView);
 			tabLay = findViewById(R.id.tabLay);
 			timeLeft = findViewById(R.id.timeLeftTxt);
 		} else {
-			setContentView(conLay);
+			setContentView(gameView);
 		}
 		timeLeft.setText(String.valueOf(TIME_FOR_ANSWER));
 
-		TableRow tabRow = new TableRow(this);
-		ConstraintLayout rowConLay = new ConstraintLayout(this);
-		TextView letter = new TextView(this);
-		EditText category = new EditText(this);
-		LinearLayout answersLay = new LinearLayout(this);
-		TextView points = new TextView(this);
-		ImageView vertLine = new ImageView(this);
+		TableRow tabRow = new TableRow(this);	// 1 Row = 1 round
+		ConstraintLayout rowConLay = new ConstraintLayout(this);	// Layout to fill the TableRow
+		TextView letter = new TextView(this);	//	random letter
+		EditText category = new EditText(this);	//	answer
+		LinearLayout answersLay = new LinearLayout(this);	//	Layout containing all the answers
+		LinearLayout categoryAndPointsLay = new LinearLayout(this);	//	Layout with a particular answer and points form it
+		TextView points = new TextView(this);	//	points scored in a particular round
+		TextView categoryPoints = new TextView(this);	// points scored on each answer
+		TableRow vertLineRow = new TableRow(this);	// row for the vertical line
+		ImageView vertLine = new ImageView(this);	// vertical line between each round
 
 		tabLay.addView(tabRow);
 		tabRow.addView(rowConLay);
@@ -111,13 +127,14 @@ public class StartGame extends AppCompatActivity {
 		letter.setId(View.generateViewId());
 
 		answersLay.setId(View.generateViewId());
+		categoryAndPointsLay.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1f));
+		categoryAndPointsLay.setOrientation(LinearLayout.VERTICAL);
 
 		category.setMaxLines(2);
 		category.setMaxWidth((int) getResources().getDisplayMetrics().density * 312);
 		category.setTextSize(30);
 		category.setTextColor(Color.BLACK);
 		category.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-		category.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1f));
 		InputFilter[] filterArray = new InputFilter[3];
 		filterArray[0] = new InputFilter.LengthFilter(256);
 		filterArray[1] = new InputFilter.AllCaps();
@@ -132,6 +149,9 @@ public class StartGame extends AppCompatActivity {
 		category.setFilters(filterArray);
 		AutofitHelper.create(category);
 
+		categoryPoints.setTextColor(Color.BLACK);
+		categoryPoints.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+		categoryPoints.setTextSize(15);
 
 		points.setTextSize(30);
 		points.setTextColor(Color.BLACK);
@@ -140,7 +160,9 @@ public class StartGame extends AppCompatActivity {
 		rowConLay.addView(letter);
 		rowConLay.addView(answersLay);
 		rowConLay.addView(points);
-		answersLay.addView(category);
+		answersLay.addView(categoryAndPointsLay);
+		categoryAndPointsLay.addView(category);
+		categoryAndPointsLay.addView(categoryPoints);
 
 		ConstraintSet conSet = new ConstraintSet();
 		conSet.clone(rowConLay);
@@ -159,7 +181,6 @@ public class StartGame extends AppCompatActivity {
 		int dividerHeight = (int) getResources().getDisplayMetrics().density * 2;
 		vertLine.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, dividerHeight));
 
-		TableRow vertLineRow = new TableRow(this);
 		ConstraintLayout vertLineLayout = new ConstraintLayout(this);
 		vertLineLayout.setLayoutParams(new TableRow.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, ConstraintLayout.LayoutParams.WRAP_CONTENT, 1f));
 		vertLineLayout.addView(vertLine);
@@ -177,11 +198,19 @@ public class StartGame extends AppCompatActivity {
 					tabLay.addView(vertLineRow);
 
 					if(category.getText() != null && Objects.requireNonNull(category.getText()).length() >= 1 && Objects.requireNonNull(category.getText()).charAt(0) == chosenLetter) {
-						points.setText("15");
+						categoryPoints.setText("15");
 					} else {
-						points.setText("0");
+						categoryPoints.setText("0");
 					}
-					setContentView(R.layout.activity_start_game);
+					points.setText(categoryPoints.getText());
+					Handler endOfRoundHandler = new Handler();
+					Runnable endOfRoundRunnable = new Runnable() {
+						@Override
+						public void run() {
+							setContentView(R.layout.activity_start_game);
+						}
+					};
+					endOfRoundHandler.postDelayed(endOfRoundRunnable, 5000);
 				}
 			}
 		};
